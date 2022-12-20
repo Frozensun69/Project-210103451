a08fce63539df088efa7c661f400bd73920acc1d
Frozensun69 <61652215+Frozensun69@users.noreply.github.com>
2022-12-20 11:16:56 +0600

    Add files via upload

---
+++ Final product/login1.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$name = $_POST['name'];
+$password = $_POST['password'];
+
+$sql = "select * from login where email like '$name' and password like '$password'";
+
+$result = $conn->query($sql);
+
+$count =mysqli_num_rows($result);
+
+if($count > 0) {
+  
+    $row=mysqli_fetch_assoc($result);
+    
+    header("Location: main.html");
+    $row['message'] = 'Succeess!!';
+    echo $row['message'];
+
+    
+}else {
+    $row['message'] = 'Name or password is incorrect';
+    header("Location: main.html");
+    echo $row['message'];
+
+    
+    
+}
+
+?>
